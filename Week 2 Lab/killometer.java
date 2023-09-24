1bf999d27c5d8558e16d13dae6c0f8703b6df3f7
Shadhujan <145929070+Shadhujan@users.noreply.github.com>
2023-09-24 23:39:52 +0530

    Add files via upload

---
+++ Week 2 Lab/killometer.java
@@ -0,0 +1,12 @@
+class killometer
+{
+    public static void main(String[] args)
+    {
+        int miles,yards;
+        
+        miles=26;
+        yards=385;
+
+        
+    }
+}
---
+++ Week 2 Lab/stringExpression.java
@@ -0,0 +1,21 @@
+class stringExpression{
+    public static void main(String[] args){
+        String str;
+        int num1,num2;
+
+        num1=12;
+        num2=26;
+
+        str="The Sum "+ num1+ num2;
+        System.out.println(str);
+
+        str="The sum "+ (num1+num2);
+        System.out.println(str);
+
+        str= num1+ num2 +" is the sum";
+        System.out.println(str);
+        
+        str="The sum of "+num1+" and "+num2+" is = "+(num1+num2);
+        System.out.println(str);
+    }
+}
